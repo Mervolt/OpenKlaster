388ca44cb65df848184931e6272fcfa49b74500f
Mervolt <michal.makson@gmail.com>
2020-05-12 14:06:27 +0200

    Initial demo commit

---
+++ OpenKlaster/OpenKlasterCore/src/main/java/vertx/CommonVerticle.java
@@ -0,0 +1,10 @@
+package vertx;
+
+import io.vertx.core.AbstractVerticle;
+
+public class CommonVerticle extends AbstractVerticle {
+    @Override
+    public void start() throws Exception {
+        super.start();
+    }
+}
