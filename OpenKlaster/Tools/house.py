4e890f9dad495660a151e0c3c16740764cd7b4f3
ravgawel <ravgawel@gmail.com>
2020-08-23 16:55:56 +0200

    add config file

--- OpenKlaster/Tools/house.py
+++ OpenKlaster/Tools/house.py
@@ -5,6 +5,9 @@ import numpy
 import datetime
 import random
 import requests
+import yaml
+
+from datetime import date
 
 headers = {'content-type': 'application/json'}
 i = 0
@@ -29,8 +32,11 @@ def load_power_function(x):
     return numpy.sin(numpy.pi / 6 * x - 2)
 
 
-def get_json_object(id, value):
-    return {"installationId": id, 'value': round(value, 3)}
+def get_json_object(id, value, timestamp = None):
+    if timestamp == None:
+        return {"installationId": id, 'value': round(value, 3)}
+    else:
+        return {"installationId": id, 'value': round(value, 3), 'timestamp': timestamp}
 
 
 def get_params(token):
@@ -39,7 +45,7 @@ def get_params(token):
 
 def post_power_production(params, obj):
     global headers
-    url = 'http://localhost:8082/api/1/powerProduction'
+    url = cfg["powerProduction"]
 
     x = requests.post(url, data=json.dumps(obj), headers=headers, params=params)
 
@@ -51,7 +57,7 @@ def post_power_production(params, obj):
 
 def post_power_consumption(params, obj):
     global headers
-    url = 'http://localhost:8082/api/1/powerConsumption'
+    url = cfg["powerConsumption"]
 
     x = requests.post(url, data=json.dumps(obj), headers=headers, params=params)
 
@@ -77,11 +83,38 @@ def house():
 
     threading.Timer(1, house).start()
 
+def fill_up(hour, minute, second):
+    global period_of_time, i, production_power_sum, load_power_sum, id
+    production_power_sum += count_function(production_power_function, hour + (minute * 60 + second) / 3600)
+    load_power_sum += count_function(load_power_function, hour + (minute * 60 + second) / 3600)
+
+    if i == period_of_time:
+        if hour < 10:
+            hour = "0" + str(hour)
+        else:
+            hour = str(hour)
+
+        if minute < 10:
+            minute = "0" + str(minute)
+        else:
+            minute = str(minute)
+
+        if second < 10:
+            second = "0" + str(second)
+        else:
+            second = str(second)
+
+        now = str(date.today()) + " " + hour + ":" + str(minute) + ":" + str(second)
+
+        post_power_production(get_params(token), get_json_object(id, production_power_sum / (i + 1), now))
+        post_power_consumption(get_params(token), get_json_object(id, load_power_sum / (i + 1), now))
+        i = 0
+        production_power_sum = 0
+        load_power_sum = 0
+    i += 1
 
 if __name__ == '__main__':
-    global token
-    global id
-    global period_of_time
+    global token, id, period_of_time
     if len(sys.argv) < 4:
         print(
             "As a parameter, enter a token, installationId and number specifying every how many seconds post request should be sent.")
@@ -90,4 +123,14 @@ if __name__ == '__main__':
         token = sys.argv[1]
         id = sys.argv[2]
         period_of_time = int(sys.argv[3])
-        house()
+
+        with open("house_config.yml", "r") as ymlfile:
+            cfg = yaml.safe_load(ymlfile)
+
+        if cfg["mode"] == "STANDARD":
+            house()
+        elif cfg["mode"] == "FILLUP":
+            for hour in range(0, 24):
+               for minute in range(0, 60):
+                   for second in range(0, 60):
+                       fill_up(hour, minute, second)
