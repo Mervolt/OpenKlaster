e0231b1054767fab9c1c20a4c6662fec7135e56e
lukasz.kowalski <lukowalski97@gmail.com>
2020-08-23 20:24:02 +0200

    small fixes

--- OpenKlaster/DigitalSignageClient/config.js
+++ OpenKlaster/DigitalSignageClient/config.js
@@ -1,16 +1,14 @@
 const config = {
     "colours" : {
-
+        "strokeColor" : "#fd8300",
+        "fillColor" : "#fdbc00"
     },
     "textTitles" : {
       "powerConsumptionTitle" : "Zużycie mocy",
       "powerProductionTitle" : "Produkcja mocy"
-
     },
     "installationId": "1",
     "apiToken": "abecadlozpiecaspadlo",
-    "strokeColor" : "#fd8300",
-    "fillColor" : "#fdbc00",
     "api": {
         "rootUrl": "http://localhost:8082",
         "powerProduction": "/power/production",
