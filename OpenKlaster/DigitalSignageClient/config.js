50bed9120eeacfad7e8e86b00e78be16d3f2c65e
lukasz.kowalski <lukowalski97@gmail.com>
2020-08-23 16:11:41 +0200

    Added basic signage client

---
+++ OpenKlaster/DigitalSignageClient/config.js
@@ -0,0 +1,10 @@
+const config = {
+    "installationId": "1",
+    "apiToken": "abecadlozpiecaspadlo",
+    "api": {
+        "rootUrl": "http://localhost:8082",
+        "powerProduction": "/power/production",
+        "powerConsumption": "/power/consumption"
+    }
+
+}
