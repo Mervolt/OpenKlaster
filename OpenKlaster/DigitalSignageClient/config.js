156bb9aa2212919a576c1f31fd9be7d7bacc65f4
lukasz.kowalski <lukowalski97@gmail.com>
2020-08-23 19:18:56 +0200

    Added config for title and colors

--- OpenKlaster/DigitalSignageClient/config.js
+++ OpenKlaster/DigitalSignageClient/config.js
@@ -1,6 +1,16 @@
 const config = {
+    "colours" : {
+
+    },
+    "textTitles" : {
+      "powerConsumptionTitle" : "Zużycie mocy",
+      "powerProductionTitle" : "Produkcja mocy"
+
+    },
     "installationId": "1",
     "apiToken": "abecadlozpiecaspadlo",
+    "strokeColor" : "#fd8300",
+    "fillColor" : "#fdbc00",
     "api": {
         "rootUrl": "http://localhost:8082",
         "powerProduction": "/power/production",
