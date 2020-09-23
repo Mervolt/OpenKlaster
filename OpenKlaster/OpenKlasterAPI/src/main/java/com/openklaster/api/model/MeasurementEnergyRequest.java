ae6ca9c7dea02d60830cc1dea51e7d582aee4a0c
ravgawel <ravgawel@gmail.com>
2020-09-23 20:51:53 +0200

    working energy

---
+++ OpenKlaster/OpenKlasterAPI/src/main/java/com/openklaster/api/model/MeasurementEnergyRequest.java
@@ -0,0 +1,21 @@
+package com.openklaster.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.openklaster.api.validation.ModelValidationErrorMessages;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MeasurementPowerRequest extends Model {
+    @NotBlank(message = ModelValidationErrorMessages.INSTALLATION_ID)
+    private String installationId;
+    private String startDate;
+    private String endDate;
+    private final String unit = "kWh";
+}
+
