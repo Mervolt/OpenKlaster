b568a386116dd26854955fbd068c066653171102
Mervolt <michal.makson@gmail.com>
2020-10-05 19:53:09 +0200

    Installation list items

---
+++ OpenKlaster/DigitalSignage/frontend/OpenKlaster/src/app/installation/installation-list-item/installation-list-item.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Installation} from "../../model/Installation";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-installation-list-item',
+  templateUrl: './installation-list-item.component.html',
+  styleUrls: ['./installation-list-item.component.css']
+})
+export class InstallationListItemComponent implements OnInit {
+  @Input() installation: Installation
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  navigateToInstallation(installationId: string) {
+    this.router.navigate(['installations', installationId]).then()
+  }
+}
