8d92611ae6f5e61b57e7be5302f54996509c30af
Mervolt <michal.makson@gmail.com>
2020-10-05 02:45:00 +0200

    Installation details box

---
+++ OpenKlaster/DigitalSignage/frontend/OpenKlaster/src/app/installation-details-box/installation-details-box.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-installation-details-box',
+  templateUrl: './installation-details-box.component.html',
+  styleUrls: ['./installation-details-box.component.css']
+})
+export class InstallationDetailsBoxComponent implements OnInit {
+  @Input() item: string
+  @Input() itemDescription: string
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
