8defa481f7f694387d7474415c59ed9747616b0a
Mervolt <michal.makson@gmail.com>
2020-11-09 11:00:14 +0100

    Minor non-logic commit

--- OpenKlasterAPI/src/main/java/com/openklaster/api/app/ApiVerticle.java
+++ OpenKlasterAPI/src/main/java/com/openklaster/api/app/ApiVerticle.java
@@ -56,14 +56,12 @@ public class ApiVerticle extends OpenklasterVerticle {
                 .requestHandler(router)
                 .listen(launchPort);
         handlers = new ArrayList<>(ctx.getBeansOfType(ApiHandler.class).values());
-        System.out.println(handlers.size());
         routerConfig(router);
     }
 
     private void routerConfig(Router router) {
         configureRouteHandler(router);
         handlers.forEach(handler -> handler.configure(router, eventBus));
-        handlers.forEach(handler -> System.out.println());
         router.route("/api/1/*").handler(StaticHandler.create("static"));
     }
 
