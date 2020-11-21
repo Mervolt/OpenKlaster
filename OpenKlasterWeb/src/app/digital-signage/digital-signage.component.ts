00a1deaad154d5a2395d40667183dff707dcfec5
Mervolt <michal.makson@gmail.com>
2020-11-21 20:50:26 +0100

    Implement choosing installation id in stepper

--- OpenKlasterWeb/src/app/digital-signage/digital-signage.component.ts
+++ OpenKlasterWeb/src/app/digital-signage/digital-signage.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AppComponent} from "../app.component";
+import {InstallationService} from "../service/installation/installation.service";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-digital-signage',
@@ -8,14 +11,24 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class DigitalSignageComponent implements OnInit {
   loading: boolean = false;
+  cookieService: CookieService;
   installationForm: FormGroup;
   installationIDs: Array<String>;
   installationId: String;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private appComp: AppComponent,
+              private installationService: InstallationService) {
+    this.cookieService = appComp.cookieService;
   }
 
   ngOnInit(): void {
+    this.loading = true;
+    this.installationService.getInstallations(this.cookieService).subscribe(response => {
+      this.installationIDs = response.map(installation => installation['_id'])
+      this.loading = false;
+    })
+
     this.installationForm = this.fb.group({
       installationId: [this.installationId, Validators.required]
     });
@@ -23,7 +36,10 @@ export class DigitalSignageComponent implements OnInit {
 
 
   installationSelected() {
-
+    if (this.installationForm.get('installationId').value != '' && this.installationForm.get('installationId').value != undefined) {
+      this.installationId = this.installationForm.get('installationId').value;
+      this.loading = true;
+    }
   }
 
   downloadFile(): void {
