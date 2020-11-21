370ce03a45f9b3fef0b4cf793646ef7d606a4d0e
Mervolt <michal.makson@gmail.com>
2020-11-21 21:26:08 +0100

    Include API tokens to Digital signage config step 1

--- OpenKlasterWeb/src/app/digital-signage/digital-signage.component.ts
+++ OpenKlasterWeb/src/app/digital-signage/digital-signage.component.ts
@@ -3,6 +3,8 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AppComponent} from "../app.component";
 import {InstallationService} from "../service/installation/installation.service";
 import {CookieService} from "ngx-cookie-service";
+import {TokenService} from "../service/token/token.service";
+import {TokenResponse} from "../token/token-panel/token-panel.component";
 
 @Component({
   selector: 'app-digital-signage',
@@ -10,30 +12,50 @@ import {CookieService} from "ngx-cookie-service";
   styleUrls: ['./digital-signage.component.css']
 })
 export class DigitalSignageComponent implements OnInit {
+  installationsLoaded: boolean = false;
+  tokensLoaded: boolean = false;
   loading: boolean = false;
   cookieService: CookieService;
   installationForm: FormGroup;
   installationIDs: Array<String>;
   installationId: String;
+  apiTokens: Array<String>;
+  apiToken: String;
 
   constructor(private fb: FormBuilder,
               private appComp: AppComponent,
-              private installationService: InstallationService) {
+              private installationService: InstallationService,
+              private tokenService: TokenService) {
     this.cookieService = appComp.cookieService;
   }
 
   ngOnInit(): void {
     this.loading = true;
-    this.installationService.getInstallations(this.cookieService).subscribe(response => {
-      this.installationIDs = response.map(installation => installation['_id'])
-      this.loading = false;
-    })
+    this.getFormOptions()
 
     this.installationForm = this.fb.group({
-      installationId: [this.installationId, Validators.required]
+      installationId: [this.installationId, Validators.required],
+      apiToken: [this.apiToken, Validators.required]
     });
   }
 
+  getFormOptions(): void {
+    this.installationService.getInstallations(this.cookieService).subscribe(response => {
+      this.installationIDs = response.map(installation => installation['_id'])
+      this.installationsLoaded = true;
+      if(this.tokensLoaded === true)
+        this.loading = false
+
+    })
+
+    this.tokenService.getTokens(this.appComp.cookieService).subscribe(response => {
+      let tokensData: TokenResponse[] = <TokenResponse[]>(response["userTokens"])
+      this.apiTokens = tokensData.map(token => token.data)
+      this.tokensLoaded = true;
+      if(this.installationsLoaded === true)
+        this.loading = false;
+    })
+  }
 
   installationSelected() {
     if (this.installationForm.get('installationId').value != '' && this.installationForm.get('installationId').value != undefined) {
